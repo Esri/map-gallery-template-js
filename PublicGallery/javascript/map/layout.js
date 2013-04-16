6cb656564b52dba3465c06fa66cd196537679be0
Matt Driscoll <mdriscoll@esri.com>
2013-04-16 10:44:32 -0700

    fix require

--- PublicGallery/javascript/map/layout.js
+++ PublicGallery/javascript/map/layout.js
@@ -9,6 +9,7 @@ dojo.require("dijit.Dialog");
 dojo.require("dojo.io.script");
 dojo.require("dojo.number");
 dojo.require("dojox.form.Rating");
+dojo.require("config.commonConfig");
 // Map Only
 dojo.require("esri.map");
 dojo.require("esri.widgets");
