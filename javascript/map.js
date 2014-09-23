8c9eab7a64425e1912c7561614f46ab0fb511b24
Matt Driscoll <MDriscoll@esri.com>
2014-09-23 10:13:30 -0700

    editable

--- javascript/map.js
+++ javascript/map.js
@@ -851,12 +851,12 @@ function(declare, lang, array, Deferred, dom, on, query, i18n, domStyle, number,
                         mapOptions: {
                             slider: true,
                             sliderStyle: "small",
-                            editable: false,
                             wrapAround180: true,
                             showAttribution: this._options.showAttribution,
                             attributionWidth: 0.40,
                             nav: false
                         },
+                        editable: false,
                         usePopupManager: true,
                         ignorePopups: false,
                         bingMapsKey: this._options.bingMapsKey,
