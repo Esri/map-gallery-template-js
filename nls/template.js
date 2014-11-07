899b939545a30c765a7642108863041acc94d6ce
Matt Driscoll <MDriscoll@esri.com>
2014-11-07 08:31:02 -0800

    nls

--- nls/template.js
+++ nls/template.js
@@ -12,14 +12,14 @@ define({
                 "bingError": "Deploying this application requires your own Bing Maps key.",
                 "noLayers": "No operational layers",
                 "noSearchResults": "Sorry, no results were found.",
-				"noGroupResults": "The group could not be found.",
+                "noGroupResults": "The group could not be found.",
                 "noMatches": "No matches found.",
                 "noMapsFound": "No maps were found."
             },
             "sidePanel": {
                 "title": "Legend",
                 "legendButton": "Legend",
-				"layersButton": "Layers", // new
+                "layersButton": "Layers", // new
                 "legendButtonTitle": "Map Legend",
                 "aboutButton": "About",
                 "aboutButtonTitle": "About Map",
@@ -35,49 +35,49 @@ define({
                 "listSwitch": "Switch to List View",
                 "loadingText": "Loading Maps&hellip;"
             },
-			"sortFields":{
-				"sortBy": "Sort by",
-				"modified": "Date",
-				"title": "Title",
-				"type": "Type",
-				"numRatings": "Ratings",
-				"avgRating": "Avg. Rating",
-				"numComments": "Comments",
-				"numViews": "Views"
-			},
-			"comments":{
-				"commentsHeader": "Comments",
-				"posted": "Posted",
-				"by": "by",
-				"deleteComment": "delete",
-				"editComment": "edit",
-				"noComments": "No comments",
-				"addCommentButton": "Add Comment",
-				"signIn": "Sign In",
-				"register": "Register",
-				"or": "or",
-				"toPost": "to post a comment."
-			},
-			"buttons":{
-				"cancel": "Cancel",
-				"submit": "Submit"
-			},
-			"rating":{
-				"signIn": "Sign In",
-				"toRate": "to rate."
-			},
-			"itemInfo":{
-				"createdLabel": "created",
-				"ratingsLabel": "rating",
-				"ratingsLabelPlural": "ratings",
-				"viewsLabel": "view",
-				"viewsLabelPlural": "views",
-				"commentsLabel": "comment",
-				"commentsLabelPlural": "comments",
-				"modifiedLabel": "Last Modified",
-				"by": "by",
-				"separator": ","
-			},
+            "sortFields": {
+                "sortBy": "Sort by",
+                "modified": "Date",
+                "title": "Title",
+                "type": "Type",
+                "numRatings": "Ratings",
+                "avgRating": "Avg. Rating",
+                "numComments": "Comments",
+                "numViews": "Views"
+            },
+            "comments": {
+                "commentsHeader": "Comments",
+                "posted": "Posted",
+                "by": "by",
+                "deleteComment": "delete",
+                "editComment": "edit",
+                "noComments": "No comments",
+                "addCommentButton": "Add Comment",
+                "signIn": "Sign In",
+                "register": "Register",
+                "or": "or",
+                "toPost": "to post a comment."
+            },
+            "buttons": {
+                "cancel": "Cancel",
+                "submit": "Submit"
+            },
+            "rating": {
+                "signIn": "Sign In",
+                "toRate": "to rate."
+            },
+            "itemInfo": {
+                "createdLabel": "created",
+                "ratingsLabel": "rating",
+                "ratingsLabelPlural": "ratings",
+                "viewsLabel": "view",
+                "viewsLabelPlural": "views",
+                "commentsLabel": "comment",
+                "commentsLabelPlural": "comments",
+                "modifiedLabel": "Last Modified",
+                "by": "by",
+                "separator": ","
+            },
             "mapPage": {
                 "findLocation": "Find a location",
                 "findPlaceholder": "Locate an address",
@@ -93,13 +93,13 @@ define({
                 "openInExplorer": "Explorer Online",
                 "ownerHeader": "Owner:",
                 "switchBasemap": "Switch Basemap",
-				"getMobileApp": "Get App",
-				"constraintsHeading":"Use Constraints",
-				"createdLabel":"Created",
-				"noDescription":"No description.",
-				"detailsLabel":"Details"
+                "getMobileApp": "Get App",
+                "constraintsHeading": "Use Constraints",
+                "createdLabel": "Created",
+                "noDescription": "No description.",
+                "detailsLabel": "Details"
             },
-            "filterItems":{
+            "filterItems": {
                 "show": "Show",
                 "all": "All Results",
                 "maps": "Maps",
@@ -118,24 +118,24 @@ define({
             }
         }
     }),
-"ar":1,
-"da":1,
-"de":1,
-"es":1,
-"fr":1,
-"he":1,
-"it":1,
-"ja":1,
-"ko":1,
-"lt":1,
-"nl":1,
-"nb":1,
-"pl":1,
-"pt-br":1,
-"pt-pt":1,
-"ro":1,
-"ru":1,
-"sv":1,
-"vi": 1,
-"zh-cn":1
+    "ar": 1, 
+    "da": 1, 
+    "de": 1, 
+    "es": 1, 
+    "fr": 1, 
+    "he": 1, 
+    "it": 1, 
+    "ja": 1, 
+    "ko": 1, 
+    "lt": 1, 
+    "nl": 1, 
+    "nb": 1, 
+    "pl": 1, 
+    "pt-br": 1, 
+    "pt-pt": 1, 
+    "ro": 1, 
+    "ru": 1, 
+    "sv": 1, 
+    "vi": 1, 
+    "zh-cn": 1 
 });
